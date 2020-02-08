913967a057245e9a78dc9e71e09c909e987962c8
NovaSquirrel <bushytail@novasquirrel.com>
2020-02-08 18:52:40 -0500

    More efficient OAM clearing in gameplay

--- src/main.s
+++ src/main.s
@@ -247,8 +247,6 @@ Die:
   ; Put all remaining sprites offscreen and generate the second OAM table 
   ; from the less compact version in RAM.
   setaxy16
-  ldx OamPtr
-  jsl ppu_clear_oam
   jsl ppu_pack_oamhi
 
   ; -------------------
@@ -256,7 +254,7 @@ Die:
   ; Backgrounds and OAM can be modified only during vertical blanking.
   ; Wait for vertical blanking and copy prepared data to OAM.
   jsl WaitVblank
-  jsl ppu_copy_oam
+  jsl ppu_copy_clear_oam
 
   ; Do row/column updates if required
   lda ColumnUpdateAddress
